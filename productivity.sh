59c315f6283a836801c8f6a110f3eab700546da0
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-09-05 18:55:36 +0300

    add twitter and instagram

--- productivity.sh
+++ productivity.sh
@@ -2,5 +2,5 @@
 
 sudo bash -c 'echo "" >> /etc/hosts';
 sudo bash -c 'echo "# Added by productivity.sh" >> /etc/hosts';
-sudo bash -c 'echo "127.0.0.1 www.facebook.com www.reddit.com news.ycombinator.com" >> /etc/hosts';
+sudo bash -c 'echo "127.0.0.1 www.facebook.com www.reddit.com news.ycombinator.com twitter.com www.instagram.com" >> /etc/hosts';
 sudo bash -c 'echo ":: www.facebook.com www.reddit.com" >> /etc/hosts';
